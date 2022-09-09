7918523ae1714b0166a100b9ab343b25e517da08
WisdomFusion <WisdomFusion@gmail.com>
2022-09-09 08:54:01 +0800

    init

---
+++ components/MainHead/MainHead.js
@@ -0,0 +1,5 @@
+import {Fragment} from 'react';
+
+export default function MainHead() {
+    return <Fragment></Fragment>;
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import {Fragment} from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+    return (
+        <Fragment></Fragment>
+    );
+}
